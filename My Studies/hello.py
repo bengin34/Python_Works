e75db0ebfb9d4db8454c014fcd4e51cf17d4356e
bengin34 <becaglar3434@gmail.com>
2023-04-15 14:34:38 +0300

    first commit

---
+++ My Studies/hello.py
@@ -0,0 +1,11 @@
+def function(): # Don't be confused, we use 'def()' to create a function. 
+                # You will see it in the next lessons.
+    """
+Hi, I am the docstring of this code. 
+If you need any information about this function or module, read me. 
+It can help you understand how the module or function works.
+    """
+print(function.__doc__)
+
+example1 = 'sometimes what you say is less important than how you say it'
+print(type(example1))
